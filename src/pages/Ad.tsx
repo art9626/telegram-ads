e4de239ec2a95977111756e6e40387bfbb691437
Artyom Chelokyan <artyomchelokyan@MacBook-Pro-Artyom.local>
2024-06-26 20:16:29 +0300

    fix

--- src/pages/Ad.tsx
+++ src/pages/Ad.tsx
@@ -1,32 +1,37 @@
-import {useEffect, useMemo} from "react";
-import {BrowserNavigator, InitData, initNavigator, useInitData} from '@tma.js/sdk-react';
+import { useEffect, useMemo } from "react";
+import {
+  BrowserNavigator,
+  InitData,
+  initNavigator,
+  useInitData,
+} from "@tma.js/sdk-react";
 import { watched } from "../api/fetch";
 
-
-
 export default function Ad() {
-  const navigator = useMemo(() => initNavigator('app-navigation-state'), []);
-  const data = useInitData()
+  const navigator = useMemo(() => initNavigator("app-navigation-state"), []);
+  const data = useInitData();
   useEffect(() => {
-    document.title = 'AdsGram';
-    showAd(data, navigator)
+    document.title = "AdsGram";
+    showAd(data, navigator);
   });
 
-  return (
-    <div>Loading...</div>
-  )
+  return <div>Loading...</div>;
 }
 
-function showAd(data: InitData | undefined, navigator: BrowserNavigator<unknown>) {
-  const token = localStorage.getItem('token');
-  const AdController = window.Adsgram.init({blockId: "239", debug: true})
+function showAd(
+  data: InitData | undefined,
+  navigator: BrowserNavigator<unknown>
+) {
+  const token = localStorage.getItem("token");
+  const AdController = window.Adsgram.init({ blockId: "239", debug: true });
   AdController.show()
     .then((result: ShowPromiseResult) => {
       // TODO: send to BE
       console.log(result);
-      watched(token, data).then(() => navigator.back())
+      watched(token, data).then(() => navigator.back());
     })
     .catch((result: ShowPromiseResult) => {
       AdController.destroy();
       console.error(result);
-    })
+    });
+}
