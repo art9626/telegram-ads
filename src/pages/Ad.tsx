2545d616fe6f3e5cb9e2378dead84ec7ed380eca
Artyom Chelokyan <artyomchelokyan@MacBook-Pro-Artyom.local>
2024-06-26 20:11:36 +0300

    fix navigation

--- src/pages/Ad.tsx
+++ src/pages/Ad.tsx
@@ -1,58 +1,32 @@
-import { useEffect } from "react";
-import { InitData, useInitData } from "@tma.js/sdk-react";
-import { NavigateFunction, useNavigate } from "react-router-dom";
-import { watched } from "../api/fetch.ts";
+import {useEffect, useMemo} from "react";
+import {BrowserNavigator, InitData, initNavigator, useInitData} from '@tma.js/sdk-react';
+import { watched } from "../api/fetch";
 
-declare global {
-  interface Window {
-    Adsgram: AdsgramProps;
-  }
+
+
+export default function Ad() {
+  const navigator = useMemo(() => initNavigator('app-navigation-state'), []);
+  const data = useInitData()
+  useEffect(() => {
+    document.title = 'AdsGram';
+    showAd(data, navigator)
+  });
+
+  return (
+    <div>Loading...</div>
+  )
 }
 
-interface AdsgramProps {
-  init: ({
-    blockId,
-    debug,
-  }: {
-    blockId: string;
-    debug: boolean;
-  }) => AdControllerProps;
-}
-
-interface AdControllerProps {
-  show: () => Promise<ShowPromiseResult>;
-  destroy: () => void;
-}
-
-interface ShowPromiseResult {
-  done: boolean; // true if user watch till the end, otherwise false
-  description: string; // event description
-  state: "load" | "render" | "playing" | "destroy"; // banner state
-  error: boolean; // true if event was emitted due to error, otherwise false
-}
-
-function showAd(data: InitData | undefined, navigate: NavigateFunction) {
-  const token = localStorage.getItem("token");
-  const AdController = window.Adsgram.init({ blockId: "239", debug: true });
+function showAd(data: InitData | undefined, navigator: BrowserNavigator<unknown>) {
+  const token = localStorage.getItem('token');
+  const AdController = window.Adsgram.init({blockId: "239", debug: true})
   AdController.show()
     .then((result: ShowPromiseResult) => {
       // TODO: send to BE
       console.log(result);
-      watched(token, data).then(() => navigate("/"));
+      watched(token, data).then(() => navigator.back())
     })
     .catch((result: ShowPromiseResult) => {
       AdController.destroy();
       console.error(result);
-    });
-}
-
-export default function Ad() {
-  const data = useInitData();
-  const navigate = useNavigate();
-  useEffect(() => {
-    document.title = "AdsGram";
-    showAd(data, navigate);
-  });
-
-  return <div>Loading...</div>;
-}
+    })
