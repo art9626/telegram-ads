6c47f0db8b42b42635c9b77caa0bffbd731259c2
ralist <danilov@ralist.ru>
2024-06-29 14:13:02 +0400

    progress for level

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -10,9 +10,6 @@ export default function Main() {
     queryFn: getUser,
   });
 
-  const xpProgress = (user?.game_data.xp || 0) / (user?.game_data.xp_to_next_level || 1)
-  console.log(user?.game_data.xp_to_next_level);
-  console.log(user?.game_data.xp);
   return (
     <>
       <Text size="9" as="div" align="center">{(user?.game_data.balance || 0) / 10e9}</Text>
@@ -21,7 +18,7 @@ export default function Main() {
         <Box width="50%">
           <Text as="div" align="center">CurrentXP: {user?.game_data.xp}</Text>
           <Text as="div" align="center">NextXP: {user?.game_data.xp_to_next_level}</Text>
-          <Progress value={xpProgress} max={100} size="3" />
+          <Progress value={(user?.game_data.xp || 0) / (user?.game_data.xp_to_next_level || 1)} max={100} size="3" />
         </Box>
       </Flex>
       <Flex align={"center"} direction={"column"}>
