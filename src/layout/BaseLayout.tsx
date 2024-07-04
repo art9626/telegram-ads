d21b656ebb269e22e595fc94e4c365178e301b91
Artyom Chelokyan <artyomchelokyan@MacBook-Pro-Artyom.local>
2024-07-04 19:45:25 +0300

    fixed container height

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Flex } from "@radix-ui/themes";
 import Header from "../components/Header";
+import { useViewport } from "@tma.js/sdk-react";
 
 export default function BaseLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const viewPort = useViewport();
+
   return (
-    <Flex direction="column" px="2" height="100dvh">
+    <Flex
+      direction="column"
+      px="2"
+      height={
+        viewPort ? `${viewPort.stableHeight}px` : "var(--tg-viewport-height)"
+      }
+
+      // style={{height: }}
+    >
+      {/* <Flex direction="column" px="2" height="var(--tg-viewport-height)"> */}
       <Header />
       <main
         style={{
