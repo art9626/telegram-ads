fff44f6d3e91bdba0d1ed8454038fe5d0cc8e49c
ralist <danilov@ralist.ru>
2024-07-07 20:26:11 +0400

    add achievements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,8 @@
 import { TonConnectButton } from "@tonconnect/ui-react";
-import GameInfo from "./GameInfo.tsx";
 
 export default function Header() {
   return (
     <header>
-      <GameInfo />
       <TonConnectButton />
     </header>
   );
