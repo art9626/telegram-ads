3b871694c8fc78e03161fac22446df61cf8dc788
Artyom Chelokyan <artyomchelokyan@MacBook-Pro-Artyom.local>
2024-07-15 18:03:25 +0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,12 @@ import "./mockEnv.ts";
 import "./styles.css";
 import { initViewport } from "@telegram-apps/sdk";
 
-if (import.meta.env.PROD) {
-  const [viewport] = initViewport();
-  viewport.then(({ expand }) => {
-    expand();
-  });
-}
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+// if (import.meta.env.PROD) {
+const [viewport] = initViewport();
+viewport.then((data) => {
+  data.expand();
+  ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+});
+// } else {
+//   ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+// }
