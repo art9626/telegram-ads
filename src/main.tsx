3d7f23c00e99a1f858398ce32d75d3b9cbcaffb6
Artyom Chelokyan <artyomchelokyan@MacBook-Pro-Artyom.local>
2024-07-16 16:11:05 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,12 @@ import "./mockEnv.ts";
 import "./styles.css";
 import { initViewport } from "@telegram-apps/sdk";
 
-// if (import.meta.env.PROD) {
-const [viewport] = initViewport();
-viewport.then((data) => {
-  data.expand();
+if (import.meta.env.DEV) {
   ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
-});
-// } else {
-//   ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
-// }
+} else {
+  const [viewport] = initViewport();
+  viewport.then((data) => {
+    data.expand();
+    ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+  });
+}
