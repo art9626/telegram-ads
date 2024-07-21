d6424ebe46fe6d7b877f3163f7d56ffefe5917db
Artyom Chelokyan <artyomchelokyan@MacBook-Pro-Artyom.local>
2024-07-21 15:24:37 +0300

    fixed header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
-
 import s from "./header.module.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className={s.header}>
       <div className={s.logo}>AdVenture(beta)</div>
-      <div className={s.info}><Link to="/info">Info</Link></div>
+      <nav>
+        <Link to="/info" className={s.infoLink}>
+          Info
+        </Link>
+      </nav>
       {/*<TonConnectButton />*/}
     </header>
   );
