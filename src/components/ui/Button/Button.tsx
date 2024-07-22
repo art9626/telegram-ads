49c2470628023f967327d98a5e457986f9221f32
Artyom Chelokyan <artyomchelokyan@MacBook-Pro-Artyom.local>
2024-07-22 18:18:52 +0300

    fixed Button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import classNames from "classnames";
 import s from "./button.module.css";
 
-export default function Button({
-  children,
-  className,
-  ...props
-}: React.PropsWithChildren<React.ButtonHTMLAttributes<HTMLButtonElement>>) {
+function ButtonComponent(
+  {
+    className,
+    ...props
+  }: React.PropsWithChildren<React.ButtonHTMLAttributes<HTMLButtonElement>>,
+  ref: React.Ref<HTMLButtonElement>
+) {
   return (
-    <button className={classNames(className, s.button)} {...props}>
-      {children}
-    </button>
+    <button className={classNames(className, s.button)} {...props} ref={ref} />
   );
 }
+
+const Button = React.forwardRef(ButtonComponent);
+export default Button;
