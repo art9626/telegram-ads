7e600c1c4c9d6fcfdb3a1d5eda24c560bccfa04d
Artyom Chelokyan <artyomchelokyan@MacBook-Pro-Artyom.local>
2024-07-24 18:12:22 +0300

    added animation for balance

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,25 +1,49 @@
+import React from "react";
+import { useSpringValue, animated } from "@react-spring/web";
 import { useUser } from "../../providers/UserProvider";
-import s from "./userInfo.module.css";
 import { numberSeparatedBySpaces } from "../../utils/convert";
+import s from "./userInfo.module.css";
 // import LevelProgress from "../LevelProgress.tsx";
 
 export default function UserInfo() {
   const { data: user } = useUser();
 
-  // const progress =
-  //   ((user?.game_data.xp || 0) * 100) / (user?.game_data.xp_to_next_level || 1);
-
-  const balance = numberSeparatedBySpaces(
-    (user?.game_data.balance ?? 0) / 10e9
-  );
-
   return (
     <div className={s.info}>
-      <div className={s.balance}>
-        <span>{balance}</span>
-      </div>
+      <BalanceWithMemo />
       <span className={s.level}>Level {user?.game_data.level}</span>
       {/* <LevelProgress/> */}
     </div>
   );
 }
+
+function Balance() {
+  const { data: user } = useUser();
+  const getBalance = () => (user?.game_data.balance ?? 0) / 10e9;
+  const [startBalance] = React.useState(() => getBalance());
+  const balance = getBalance();
+
+  return (
+    <div className={s.balance}>
+      <Number start={startBalance} n={balance} />
+    </div>
+  );
+}
+
+function Number({ start, n }: { start: number; n: number }) {
+  const number = useSpringValue(start, {
+    config: { mass: 1, tension: 20, friction: 10 },
+  });
+
+  React.useEffect(() => {
+    number.start(n);
+  }, [n, number]);
+
+  return (
+    <animated.div className={s.balance}>
+      {number.to((n) => numberSeparatedBySpaces(n.toFixed(0)))}
+    </animated.div>
+  );
+}
+
+const BalanceWithMemo = React.memo(Balance);
